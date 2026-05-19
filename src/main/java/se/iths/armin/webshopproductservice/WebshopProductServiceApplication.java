1fe79204d2af4f90863b8d3b8cc4601987381251
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-19 11:13:56 +0200

    add gh actions

---
+++ src/main/java/se/iths/armin/webshopproductservice/WebshopProductServiceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshopproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/armin/webshopproductservice/WebshopProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshopproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
