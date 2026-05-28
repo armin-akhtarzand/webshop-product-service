26e9bc99a784e90850c873fea76a8a3c435d3630
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-28 19:32:12 +0200

    add annotation ActiveProfiles in test

--- src/test/java/se/iths/armin/webshopproductservice/integration/ProductIntegrationTest.java
+++ src/test/java/se/iths/armin/webshopproductservice/integration/ProductIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import se.iths.armin.webshopproductservice.dto.ProductRequestDto;
 import se.iths.armin.webshopproductservice.dto.ProductStockRequest;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ProductIntegrationTest {
 
     @Autowired
